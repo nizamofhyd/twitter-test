e4c94866d163465351ad90458cb9e8e1c9480404
Arun <arun_kumar@discovery.com>
2019-03-13 15:57:59 +0000

    Added recycler implementation

--- app/src/main/java/com/arun/twittertest/activity/MainActivity.kt
+++ app/src/main/java/com/arun/twittertest/activity/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.arun.twittertest.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.arun.domain.models.Tweet
 import com.arun.twittertest.R
+import com.arun.twittertest.adapters.TwitterListAdapter
+import com.arun.twittertest.util.Status
 import com.arun.twittertest.viewmodels.TwitterFeedViewModel
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
@@ -21,12 +29,42 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     private lateinit var twitterFeedViewModel: TwitterFeedViewModel
 
+    private lateinit var twitterListAdapter: TwitterListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         twitterFeedViewModel = ViewModelProviders.of(this, viewModelFactory)
             .get(TwitterFeedViewModel::class.java)
+        bind()
+        fetchData()
     }
 
     override fun supportFragmentInjector() = dispatchingAndroidInjector
+
+    fun bind() {
+        twitterRecyclerView.layoutManager = LinearLayoutManager(this)
+        twitterListAdapter = TwitterListAdapter(emptyList())
+        twitterRecyclerView.adapter = twitterListAdapter
+        twitterFeedViewModel.tweetsLiveData.observe(this, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    it.model as List<Tweet>
+                    twitterListAdapter.updateTweets(it.model)
+                }
+                Status.ERROR -> {
+                    val dialog = AlertDialog.Builder(this)
+                        .setTitle(R.string.dialog_title)
+                        .setMessage(R.string.dialog_error_msg)
+                        .setPositiveButton(R.string.dialog_positive) { dialog, which -> dialog.dismiss() }
+                        .show()
+
+                }
+            }
+        })
+    }
+
+    fun fetchData() {
+        twitterFeedViewModel.getTwitterFeed()
+    }
 }
--- app/src/main/java/com/arun/twittertest/adapters/TwitterListAdapter.kt
+++ app/src/main/java/com/arun/twittertest/adapters/TwitterListAdapter.kt
@@ -1,2 +1,50 @@
 package com.arun.twittertest.adapters
 
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.arun.domain.models.Tweet
+import com.arun.twittertest.R
+
+class TwitterListAdapter(private var listOfTweet: List<Tweet>) :
+    RecyclerView.Adapter<TwitterListAdapter.TwitterViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TwitterViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.tweet_list_item, parent, false)
+        return TwitterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TwitterViewHolder, position: Int) {
+        holder.bind(listOfTweet.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return listOfTweet.size
+    }
+
+    fun updateTweets(tweets: List<Tweet>) {
+        listOfTweet = tweets
+        notifyDataSetChanged()
+    }
+
+    class TwitterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(tweet: Tweet) {
+            view.findViewById<TextView>(R.id.date).text = tweet.date
+
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                view.findViewById<TextView>(R.id.description).text =
+                    Html.fromHtml(tweet.description, Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                view.findViewById<TextView>(R.id.description).text = Html.fromHtml(tweet.description)
+            }
+        }
+
+
+    }
+}
